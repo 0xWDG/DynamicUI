8298d5890f7a0af36db4956497ef69c1ad981c4f
Wesley de Groot <email@wesleydegroot.nl>
2025-10-27 16:53:14 +0100

    Update formatting in Package.swift

--- Package.swift
+++ Package.swift
@@ -16,15 +16,18 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "DynamicUI",
-            targets: ["DynamicUI"])
+            targets: ["DynamicUI"]
+        )
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "DynamicUI"),
+            name: "DynamicUI"
+        ),
         .testTarget(
             name: "DynamicUITests",
-            dependencies: ["DynamicUI"])
+            dependencies: ["DynamicUI"]
+        )
     ]
 )
